33a2885dca441acef9d286044f9aaad843780b25
rainintime7 <wangsh256@outlook.com>
2024-07-01 13:18:33 +0200

    add json example of visslides

--- my-react-app/src/examples.js
+++ my-react-app/src/examples.js
@@ -6,7 +6,7 @@ export default [
       "class",
       "diagram"
     ],
-    "figure": "classDiagram.png",
+    "figure": "classDiagram.svg",
     "userCode": "class",
     "renderCode": "classDiagram\n    title Class Diagram Example\n    class Class1\n    class Class2\n    Class1 --> Class2"
   },
@@ -17,7 +17,7 @@ export default [
       "state",
       "diagram"
     ],
-    "figure": "stateDiagram.png",
+    "figure": "stateDiagram.svg",
     "userCode": "state",
     "renderCode": "stateDiagram-v2\n    [*] --> State1\n    State1 --> [*]"
   },
@@ -28,8 +28,19 @@ export default [
       "timeline",
       "history"
     ],
-    "figure": "timeline.png",
+    "figure": "timeline.svg",
     "userCode": "timeline",
     "renderCode": "timeline\ntitle History of Social Media Platform\n2002 : LinkedIn\n2004 : Facebook\n     : Google\n2005 : Youtube\n2006 : Twitter"
+  },
+  {
+    "id": "visslidesDiagram",
+    "title": "VisSlides Diagram",
+    "tags": [
+      "VisSlides",
+      "Diagram"
+    ],
+    "figure": "visslides.svg",
+    "userCode": "visslides",
+    "renderCode": "visslides\npage\narray\n@ 1 &blue&\n@ 2\n@ 3\nmatrix\n@ \n1,2\n3,4\n@\npage\narray\n@ 1\n@ 2 &blue&\n@ 3\n@ 4\nmatrix\n@\n1,2,3\n4,5,6\n@\npage\narray\n@ 1\n@ 2 \n@ 3 &blue&\n@ 4\nmatrix\n@ \n1,2,3\n4,5,6\n@\npage\narray\n@ 1\n@ 2 \n@ 3 \n@ 4 &blue&\nmatrix\n@ \n1,2,3\n4,5,6\n@"
   }
 ];
