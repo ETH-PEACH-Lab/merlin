1f91f6835957a3eb067cba537e2d9aa4d8573e96
Megan <megan.marty@inf.ethz.ch>
2025-07-25 21:59:24 +0200

    fix: Remove debugging code

--- src/parser/parseText.mjs
+++ src/parser/parseText.mjs
@@ -7,6 +7,5 @@ export default function parseText(input) {
     const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
     parser.feed(input);
     const parsedData = parser.results[0];  // Parsed output
-    console.log(parsedData);
     return parsedData;
 }
