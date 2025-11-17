0273ec46216536dceecdd6ccb656cd26e718ec65
Megan <megan.marty@inf.ethz.ch>
2025-11-17 14:59:50 +0100

    feat: Change header components to look more like docs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,38 @@
 import React from 'react';
-import { Button, Box, Toolbar, Typography, AppBar, Grid, IconButton } from '@mui/material';
+import { Button, Box, Toolbar, Typography, AppBar, Grid, IconButton, SvgIcon } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub"
+import { merlinIcon } from './CustomIcons';
 
 const Header = () => {
     return (
         <AppBar position="static" sx={{
             justifyContent: "space-between",
             flexDirection: "row",
-            padding: '2px 15px',
-            borderBottom: '1px solid #444',
-            backgroundColor: '#000'
+            padding: "2px 15px",
+            borderBottom: "1px solid #444",
+            backgroundColor: "#000"
         }}>
 
-            <Box display="flex" flexGrow={1} alignItems={'center'}>
-                <Typography sx={{ fontWeight: 'bold', textTransform: 'uppercase'}}>Merlin Editor 🧙🏼‍♂️🪄 </Typography>
+            <Box display="flex" flexGrow={1} alignItems={"center"}>
+                <SvgIcon component={merlinIcon}></SvgIcon>
+                <Typography sx={{ fontWeight: "bold" }}>
+                    &nbsp;
+                    Merlin
+                </Typography>
             </Box>
             <Box display="flex">
                 <Toolbar disableGutters >
-                    <Grid container justifyContent="space-between" alignItems="center">
-                    <IconButton
-            href="https://github.com/ETH-PEACH-Lab/intuition-vis-test"
-            target="_blank"
-            rel="noreferrer"
-            size="large"
-          >
-            <GitHubIcon />
-          </IconButton>
-                        <Button variant="text" color="inherit" href='https://eth-peach-lab.github.io/merlin-about/' target='_blank'>About</Button>
-                        <Button variant="text" color="inherit" href='https://eth-peach-lab.github.io/merlin-docs/' target="_blank">Docs</Button>
-                        <Button variant="text" color="inherit" href="https://eth-peach-lab.github.io/merlin-tutorial/" target="_blank">Tutorial</Button>
-                        <Button variant="text" color="inherit" href="https://eth-peach-lab.github.io/intuition-visualisation/" target="_blank">Intuition Viewer</Button>
+                    <Grid container justifyContent="space-between" alignItems="center" textTransform={"lowercase"}>
+                        <IconButton
+                            href="https://github.com/ETH-PEACH-Lab/merlin"
+                            target="_blank"
+                            rel="noreferrer"
+                            size="large"
+                        >
+                            <GitHubIcon />
+                        </IconButton>
+                        <Button sx={{ textTransform: 'capitalize' }} variant="text" color="inherit" href="https://eth-peach-lab.github.io/merlin-docs/docs/getting-started" target="_blank">Documentation</Button>
+                        <Button sx={{ textTransform: 'capitalize' }} variant="text" color="inherit" href="https://eth-peach-lab.github.io/merlin-docs/development" target="_blank">Development</Button>
                     </Grid>
                 </Toolbar>
             </Box>
